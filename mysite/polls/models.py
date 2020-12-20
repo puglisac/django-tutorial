42238b08c4eb35428fce12658b7c578f45984c20
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-12-20 11:19:10 -0600

    added test for was_published_recently

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,7 +13,8 @@ class Question(models.Model):
 
 # returns true if pub_date is less than 1 day old
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 # Choice model
 class Choice(models.Model):
