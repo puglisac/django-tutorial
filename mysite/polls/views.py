af790c8f30e51892fa7d220ff44ff085d0c17e02
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-12-20 12:42:44 -0600

    added filter to exclude questions with now choices

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,7 +16,8 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         questions = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-        return questions
+        question_set = [q for q in questions if q.choice_set.all()]
+        return question_set
 
 class DetailView(generic.DetailView):
     model = Question
